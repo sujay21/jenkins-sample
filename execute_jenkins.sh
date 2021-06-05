a4f71bf7759586c78a74fe17ff4f1215690f6843
sujay21 <pawarsujay.85@gmail.com>
2021-06-05 20:15:35 +0530

    Create Scripts

---
+++ execute_jenkins.sh
@@ -0,0 +1,8 @@
+printenv
+echo "This is simple command in jenkins job"
+echo $USER
+echo $PATH
+echo $BUILD_NUMBER
+echo $JOB_NAME
+echo "User has selected Regio as $REGION_CHOICE"
+echo "User has selected Env as $ENV_CHOICE"
